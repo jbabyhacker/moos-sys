eecc98efb49aa6e34f39e09eab1182317c565d23
jbabyhacker <jasoneklein5@gmail.com>
2019-12-04 21:46:51 -0500

    Initial MOOS binding * Ability to set .moos file * Implement callbacks for Iterate, OnNewMail, OnStartUp, and OnConnectToServer * Added example program that registers for updates to the DB as well as notifies the DB with updates

---
+++ build.rs
@@ -0,0 +1,41 @@
+use std::env;
+use std::path::PathBuf;
+
+fn main() {
+    cc::Build::new()
+        .file("src/AppCastingMOOSApp_c.cpp")
+        .cpp(true)
+        .warnings(true)
+        .extra_warnings(true)
+        .compile("moosbinding");
+
+    println!("cargo:rustc-link-lib=MOOS");
+    println!("cargo:rustc-link-lib=dylib=stdc++");
+    println!("cargo:rerun-if-changed=src/AppCastingMOOSApp_c.cpp");
+    println!("cargo:rerun-if-changed=src/AppCastingMOOSApp_c.hpp");
+
+    // The bindgen::Builder is the main entry point
+    // to bindgen, and lets you build up options for
+    // the resulting bindings.
+    let bindings = bindgen::Builder::default()
+        // The input header we would like to generate
+        // bindings for.
+        .header("src/AppCastingMOOSApp_c.hpp")
+        .clang_arg("-x")
+        .clang_arg("c++")
+        .clang_arg("-std=c++11")
+        .no_copy("MoosApp")
+        // Tell cargo to invalidate the built crate whenever any of the
+        // included header files changed.
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        // Finish the builder and generate the bindings.
+        .generate()
+        // Unwrap the Result and panic on failure.
+        .expect("Unable to generate bindings");
+
+    // Write the bindings to the $OUT_DIR/bindings.rs file.
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
+}
---
+++ examples/simple.rs
@@ -0,0 +1,78 @@
+use moos_sys::MoosInterface;
+use std::collections::HashMap;
+use std::os::raw::c_void;
+
+// Create your struct and include MoosApp as a member.
+pub struct DemoMoosApp {
+    app: *mut moos_sys::MoosApp,
+    value: i32,
+    data: HashMap<String, moos_sys::MoosMessageData>,
+}
+
+// Initialize your struct
+impl DemoMoosApp {
+    pub fn new() -> Self {
+        DemoMoosApp {
+            app: moos_sys::MoosApp::new::<DemoMoosApp>(),
+            value: 0,
+            data: Default::default(),
+        }
+    }
+
+    pub fn do_work(&mut self) {
+        println!("doing work");
+    }
+}
+
+// Implement the MoosInterface for your struct, these are the callbacks
+// that are to be called.
+impl MoosInterface for DemoMoosApp {
+    extern "C" fn iterate(app: *mut c_void) -> bool {
+        let this_app = moos_sys::this::<DemoMoosApp>(app);
+
+        println!("Value: {}", this_app.value);
+
+        this_app.do_work();
+        let a: &mut moos_sys::MoosApp = this_app.app();
+
+        a.notify(
+            "X",
+            &moos_sys::MoosMessageData::DOUBLE(this_app.value as f64),
+        );
+        a.notify("Y", &moos_sys::MoosMessageData::STRING("test"));
+
+        true
+    }
+
+    extern "C" fn on_start_up(_app: *mut c_void) -> bool {
+        println!("onStartUp");
+        true
+    }
+
+    extern "C" fn on_connect_to_server(app: *mut c_void) -> bool {
+        println!("onConnectToServer");
+        let this_app = moos_sys::this::<DemoMoosApp>(app);
+        let d: &mut moos_sys::MoosApp = this_app.app();
+        d.register("X", 0.0);
+        d.register("Y", 0.0);
+        true
+    }
+
+    fn on_new_mail(app: *mut c_void, d: HashMap<String, moos_sys::MoosMessageData>) -> bool {
+        let this_app = moos_sys::this::<DemoMoosApp>(app);
+        this_app.data = d;
+
+        println!("setMail - {:?}", this_app.data);
+        this_app.value += 1;
+        true
+    }
+
+    fn app(&mut self) -> &'static mut moos_sys::MoosApp {
+        moos_sys::to_app(self.app)
+    }
+}
+
+fn main() {
+    let mut moos_app = DemoMoosApp::new();
+    moos_app.run("eat", "attack.moos");
+}
